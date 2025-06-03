5cf2561e6b246f6489262eb650096c92208aaea5
Christophe-THEVENET <christophethevenet2.0@gmail.com>
2025-06-03 16:22:00 +0200

    feat: get cars on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(CarRepository $repository): Response
     {
+        $cars = $repository->findAll();
+        if (empty($cars)) {
+            $this->addFlash('warning', 'No cars available at the moment.');
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'cars' => $cars
         ]);
     }
 }
